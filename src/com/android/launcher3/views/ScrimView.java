8bbe852d9f6dc915099b3d7dc6424db2f5125677
Omkar Chandorkar <gotenksIN@aosip.dev>
2021-05-13 06:54:47 +0000

    Revert "Launcher3: disable dragHandle"
    This reverts commit 46c7da6d400b80f6a063e5709630524eaa7e196a.

--- src/com/android/launcher3/views/ScrimView.java
+++ src/com/android/launcher3/views/ScrimView.java
@@ -437,7 +437,7 @@ public class ScrimView<T extends Launcher> extends View implements Insettable, O
     }
 
     protected boolean shouldDragHandleBeVisible() {
-        return false;
+        return mLauncher.getDeviceProfile().isVerticalBarLayout() || mAM.isEnabled();
     }
 
     @Override
